aa6120ca5856d6bfc728c648034fdf3e44fc69d4
jubran nassar <jubrann@spacelift.io>
2023-08-15 01:04:19 +0300

    changes permissions

--- administrative/integration.tf
+++ administrative/integration.tf
@@ -38,7 +38,7 @@ resource "aws_iam_role" "this" {
 
 resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
-  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "spacelift_aws_integration_attachment" "ec2-deployment-stack" {
