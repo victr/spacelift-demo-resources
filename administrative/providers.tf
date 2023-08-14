c2aa60242b2873371bb6e3b15bece9bac12ef2fd
jubran nassar <jubrann@spacelift.io>
2023-08-14 19:47:15 +0300

    adds aws provider

--- administrative/providers.tf
+++ administrative/providers.tf
@@ -4,7 +4,13 @@ terraform {
       source  = "spacelift-io/spacelift"
       version = "1.1.7"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.12.0"
+    }
   }
 }
 
-provider "spacelift" {}
+provider "aws" {
+  region = "us-east-1"
+}
