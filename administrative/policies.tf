803ead5c7426b794ff0f8c857ef7faa137562582
jubran nassar <jubrann@spacelift.io>
2023-08-14 18:56:58 +0300

    data resource fetch

--- administrative/policies.tf
+++ administrative/policies.tf
@@ -2,5 +2,5 @@ resource "spacelift_policy" "default-login-ploicy" {
   name = "default login"
   body = file("../policies/login/default-login.rego")
   type = "LOGIN"
-  # space_id = "demo-resources-01H7T746B176K9S5MH0N63MGPH"
+  space_id = output.id.value
 }
