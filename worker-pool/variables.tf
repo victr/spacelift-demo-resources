46531db1d1d52c012cc166884260a1cc95d7ffb2
jubran nassar <jubrann@spacelift.io>
2023-08-14 22:31:08 +0300

    comments out variables

--- worker-pool/variables.tf
+++ worker-pool/variables.tf
@@ -1,19 +1,19 @@
-variable "worker_pool_config" {
-  type = string
-}
+# variable "worker_pool_config" {
+#   type = string
+# }
 
-variable "worker_pool_private_key" {
-  type = string
-}
+# variable "worker_pool_private_key" {
+#   type = string
+# }
 
-variable "worker_pool_id" {
-  type = string
-}
+# variable "worker_pool_id" {
+#   type = string
+# }
 
-variable "worker_pool_security_groups" {
-  type = list(string)
-}
+# variable "worker_pool_security_groups" {
+#   type = list(string)
+# }
 
-variable "worker_pool_subnets" {
-  type = list(string)
-}
+# variable "worker_pool_subnets" {
+#   type = list(string)
+# }
