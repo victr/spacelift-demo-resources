5488da2b1c0a91ae862fb673a435632fa133ad75
jubran nassar <jubrann@spacelift.io>
2023-08-14 17:47:06 +0300

    fixes spacelift provider

--- providers.tf
+++ providers.tf
@@ -1,8 +1,10 @@
 terraform {
   required_providers {
     spacelift = {
-      source  = "spacelift-io/spacelift"
+      source = "spacelift-io/spacelift"
       version = "1.1.7"
     }
   }
 }
+
+provider "spacelift" {}
