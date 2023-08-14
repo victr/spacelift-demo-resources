2fadd11657f65cfe442f0154981ed3bcb93d7234
jubran nassar <jubrann@spacelift.io>
2023-08-14 22:00:39 +0300

    creates worker pool stack

--- administrative/stacks.tf
+++ administrative/stacks.tf
@@ -13,12 +13,25 @@
 # }
 
 resource "spacelift_stack" "EC2-deployment" {
-  space_id                     = "demo-resources-01H7T746B176K9S5MH0N63MGPH"
+  space_id                     = data.spacelift_current_space.this.id
   branch                       = "main"
   description                  = "this stack will deploy a basic ec2 instance"
   name                         = "EC2-deployment"
   project_root                 = "ec2-deployment"
   repository                   = "demo-resources"
+  terraform_smart_sanitization = true
+  github_enterprise {
+    namespace = "jubranNassar"
+  }
+}
+
+resource "spacelift_stack" "demo-worker-pool" {
+  space_id = data.spacelift_current_space.this.id
+  branch = "main"
+  description = "This stack will be in charge of creating our worker pool"
+  name = "demo-worker-pool"
+  project_root = "worker-pool"
+  repository = "demo-resources"
   terraform_smart_sanitization = true
   github_enterprise {
     namespace = "jubranNassar"
