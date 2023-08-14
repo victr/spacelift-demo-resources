c5747e4d4c9bea7238c99f6cfc65d7affaf0d00a
jubran nassar <jubrann@spacelift.io>
2023-08-14 19:13:10 +0300

    fetches id

--- administrative/policies.tf
+++ administrative/policies.tf
@@ -2,5 +2,5 @@ resource "spacelift_policy" "default-login-ploicy" {
   name = "default login"
   body = file("../policies/login/default-login.rego")
   type = "LOGIN"
-  space_id = data.spacelift_current_space.this
+  space_id = data.spacelift_current_space.this.id
 }
