96bf462e5de8e1d889f3b627ba544b1cb5a345f1
jubran nassar <jubrann@spacelift.io>
2023-08-14 19:44:51 +0300

    creates aws integration

---
+++ administrative/integration.tf
@@ -0,0 +1,24 @@
+# Needed for generating the correct role ARNs
+data "aws_caller_identity" "current" {}
+
+locals {
+  role_name = "my_role"
+  role_arn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.role_name}"
+}
+
+# Create the AWS integration before creating your IAM role. The integration needs to exist
+# in order to generate the external ID used for role assumption.
+resource "spacelift_aws_integration" "this" {
+  name = "aws-integration"
+
+  # We need to set the ARN manually rather than referencing the role to avoid a circular dependency
+  role_arn                       = local.role_arn
+  generate_credentials_in_worker = false
+}
+
+# data "spacelift_aws_integration_attachment_external_id" "my_stack" {
+#   integration_id = spacelift_aws_integration.this.id
+#   stack_id       = "my-stack-id"
+#   read           = true
+#   write          = true
+# }
--- administrative/policies.tf
+++ administrative/policies.tf
@@ -1,6 +1,6 @@
 resource "spacelift_policy" "default-login-ploicy" {
-  name = "default login"
-  body = file("../policies/login/default-login.rego")
-  type = "LOGIN"
+  name     = "default login"
+  body     = file("../policies/login/default-login.rego")
+  type     = "LOGIN"
   space_id = data.spacelift_current_space.this.id
 }
