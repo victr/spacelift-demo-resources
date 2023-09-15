69f7ad97858e0363c5847994df89dd5739401891
jubran nassar <jubrann@spacelift.io>
2023-09-15 16:30:19 -0400

    removes datasource

--- administrative/stacks.tf
+++ administrative/stacks.tf
@@ -1,6 +1,6 @@
 
 resource "spacelift_stack" "EC2-deployment" {
-  space_id                     = data.spacelift_current_space.this.id
+  space_id                     = "demo-resources-01H7T746B176K9S5MH0N63MGPH"
   branch                       = "main"
   description                  = "this stack will deploy a basic ec2 instance"
   name                         = "EC2-deployment"
@@ -14,7 +14,7 @@ resource "spacelift_stack" "EC2-deployment" {
 
 resource "spacelift_stack" "demo-worker-pool" {
   administrative               = true
-  space_id                     = data.spacelift_current_space.this.id
+  space_id                     = "demo-resources-01H7T746B176K9S5MH0N63MGPH"
   branch                       = "main"
   description                  = "This stack will be in charge of creating our worker pool"
   name                         = "demo-worker-pool"
