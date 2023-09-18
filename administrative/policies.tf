0a7289aca8704036b5d0b19edfcc9c2f428b908c
jubran nassar <jubrann@spacelift.io>
2023-09-18 15:26:48 -0400

    fixes space id

--- administrative/policies.tf
+++ administrative/policies.tf
@@ -9,7 +9,7 @@ resource "spacelift_policy" "check-instance-type" {
   name     = "check-instance-type"
   body     = file("../policies/plan/check-instance-type.rego")
   type     = "PLAN"
-  space_id = "demo-resources-01H7T746B176K9S5MH0N63MGPH"
+  space_id = data.spacelift_current_space.this.id
 }
 
 resource "spacelift_policy_attachment" "check-instance-type-attachment" {
@@ -21,7 +21,7 @@ resource "spacelift_policy" "require-two-approvals" {
   name     = "require-two-approvals"
   body     = file("../policies/approval/require-two-approvals.rego")
   type     = "APPROVAL"
-  space_id = "demo-resources-01H7T746B176K9S5MH0N63MGPH"
+  space_id = data.spacelift_current_space.this.id
 }
 
 resource "spacelift_policy_attachment" "require-two-approvals-attachment" {
