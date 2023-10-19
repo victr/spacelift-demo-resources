e7766453af8f8cc3815e8c3d8af4e3e0b34b8fc8
jubran nassar <jubrann@spacelift.io>
2023-10-19 17:48:22 -0400

    moves to the demo resource space

--- administrative/blueprints.tf
+++ administrative/blueprints.tf
@@ -1,7 +1,7 @@
 resource "spacelift_blueprint" "AWS-environment" {
     name        = "AWS-Environment"
     description = "Environment for AWS"
-    space       = "root"
+    space       = spacelift_space.demo-resources.id
     template    = templatefile("${path.root}./blueprints/aws_env.tftpl", { integrations = data.spacelift_aws_integrations.this.integrations })
     state       = "PUBLISHED"
     labels      = ["dev"]
