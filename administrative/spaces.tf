87781a531cff5ee2183cbea4422d4c06854f764f
jubran nassar <jubrann@spacelift.io>
2023-11-16 23:28:47 -0500

    removes depends_on

--- administrative/spaces.tf
+++ administrative/spaces.tf
@@ -1,11 +1,4 @@
 resource "spacelift_space" "demo-resources" {
   name             = "demo-resources"
   inherit_entities = true
-  depends_on = [
-    spacelift_blueprint.AWS-environment,
-    spacelift_policy.check-instance-type,
-    spacelift_policy.require-two-approvals,
-    spacelift_stack.demo-worker-pool,
-    spacelift_stack_destructor.EC2-deployment
-  ]
 }
