de1bf66df05a02a9c6229d3b2eb02acd4fb82351
jubran nassar <jubrann@spacelift.io>
2023-11-20 12:19:31 -0500

    edits provider block

--- ec2-deployment/providers.tf
+++ ec2-deployment/providers.tf
@@ -14,9 +14,4 @@ terraform {
 provider "aws" {
   alias = "second-account"
   region = "us-east-1"
-
-  assume_role {
-    role_arn = "arn:aws:iam::379163426062:role/jubran-test-role"
-    session_name = var.spacelift.run_id
-  }
 }
