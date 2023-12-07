340819d433f1d42116cf15f2b80fe7d10d22c1e3
jubran nassar <jubrann@spacelift.io>
2023-12-07 14:05:57 -0500

    change instance type

--- ec2-deployment/main.tf
+++ ec2-deployment/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "demo-instance" {
   ami           = data.aws_ami.dev_server_ami.id
-  instance_type = "t2.micro"
+  instance_type = "t3.small"
 
   tags = {
     demo = "resource"
