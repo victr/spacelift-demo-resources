bce6e976a52086203fa644002af840e0c5f8caa5
jubran nassar <jubrann@spacelift.io>
2024-05-21 16:24:14 -0400

    cleans up

--- ec2-deployment/main.tf
+++ ec2-deployment/main.tf
@@ -6,13 +6,3 @@ resource "aws_instance" "demo-instance" {
     demo = "resource"
   }
 }
-
-resource "aws_instance" "demo-instance-prod" {
-  provider      = aws.prod
-  ami           = data.aws_ami.dev_server_ami.id
-  instance_type = "t2.micro"
-
-  tags = {
-    demo = "resource"
-  }
-}
