908d0a32163a46b561bc05b37a38781379ebd385
jubran <jubrann@spacelift.io>
2024-08-27 14:08:10 -0500

    test instance type

--- ec2-deployment/main.tf
+++ ec2-deployment/main.tf
@@ -1,8 +1,11 @@
 resource "aws_instance" "demo-instance" {
   ami           = data.aws_ami.dev_server_ami.id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
     demo = "resource"
   }
+}
+
+variable "instance_type" {
 }
