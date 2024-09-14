dccd10c7dc8d877cd5b8e6035c7cafce9643a279
jubran <jubrann@spacelift.io>
2024-09-14 14:38:05 -0500

    removes policies

--- administrative/policies.tf
+++ administrative/policies.tf
@@ -1,23 +1,23 @@
-resource "spacelift_policy" "check-instance-type" {
-  name     = "check-instance-type"
-  body     = file("../policies/plan/check-instance-type.rego")
-  type     = "PLAN"
-  space_id = spacelift_space.demo-resources.id
-}
+# resource "spacelift_policy" "check-instance-type" {
+#   name     = "check-instance-type"
+#   body     = file("../policies/plan/check-instance-type.rego")
+#   type     = "PLAN"
+#   space_id = spacelift_space.demo-resources.id
+# }
 
-resource "spacelift_policy_attachment" "check-instance-type-attachment" {
-  policy_id = spacelift_policy.check-instance-type.id
-  stack_id  = module.EC2-deployment.spacelift_stack.id
-}
+# resource "spacelift_policy_attachment" "check-instance-type-attachment" {
+#   policy_id = spacelift_policy.check-instance-type.id
+#   stack_id  = module.EC2-deployment.spacelift_stack.this.id
+# }
 
-resource "spacelift_policy" "require-two-approvals" {
-  name     = "require-two-approvals"
-  body     = file("../policies/approval/require-two-approvals.rego")
-  type     = "APPROVAL"
-  space_id = spacelift_space.demo-resources.id
-}
+# resource "spacelift_policy" "require-two-approvals" {
+#   name     = "require-two-approvals"
+#   body     = file("../policies/approval/require-two-approvals.rego")
+#   type     = "APPROVAL"
+#   space_id = spacelift_space.demo-resources.id
+# }
 
-resource "spacelift_policy_attachment" "require-two-approvals-attachment" {
-  policy_id = spacelift_policy.require-two-approvals.id
-  stack_id  = spacelift_stack.EC2-deployment.id
-}
+# resource "spacelift_policy_attachment" "require-two-approvals-attachment" {
+#   policy_id = spacelift_policy.require-two-approvals.id
+#   stack_id  = spacelift_stack.EC2-deployment.id
+# }
