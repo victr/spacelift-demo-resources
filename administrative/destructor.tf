4e21b31bb6f76ba6ed8b3a5b403b9dcbf41c227b
jubran <jubrann@spacelift.io>
2024-09-14 14:21:35 -0500

    fixes more references

--- administrative/destructor.tf
+++ administrative/destructor.tf
@@ -1,7 +1,7 @@
 resource "spacelift_stack_destructor" "EC2-deployment" {
-  stack_id = module.spacelift_stack.EC2-deployment.id
+  stack_id = module.EC2-deployment.spacelift_stack.id
 }
 
 resource "spacelift_stack_destructor" "demo-worker-pool" {
-  stack_id = module.spacelift_stack.demo-worker-pool.id
+  stack_id = module.demo-worker-pool.spacelift_stack.id
 }
