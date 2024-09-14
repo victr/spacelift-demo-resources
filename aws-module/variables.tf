e3b7293c918027a612b16f29d3e099431e0fc9d7
jubran <jubrann@spacelift.io>
2024-09-14 16:03:00 -0500

    adds required variables

--- aws-module/variables.tf
+++ aws-module/variables.tf
@@ -1,13 +1,16 @@
 variable "instance_type" {
   type        = string
   description = "Specify instance type."
+  default     = "t2.micro"
 }
 
 variable "instance_name" {
   type        = string
   description = "Specify a name for this instance"
+  default     = "dev"
 }
 
 variable "aws_region" {
-  type = string
+  type    = string
+  default = "us-east-1"
 }
