a1c933eb6ecc75665387f19d1e03bf7002bc7883
jubran <jubrann@spacelift.io>
2024-09-14 19:02:24 -0500

    fixes naming convention

--- administrative/integration-attachements.tf
+++ administrative/integration-attachements.tf
@@ -1,11 +1,11 @@
-resource "spacelift_aws_integration_attachment" "this" {
+resource "spacelift_aws_integration_attachment" "ec2-deployment" {
   integration_id = data.spacelift_aws_integration.this.id
   stack_id       = data.spacelift_stack.ec2-deployment.id
   read           = true
   write          = true
 }
 
-resource "spacelift_aws_integration_attachment" "this" {
+resource "spacelift_aws_integration_attachment" "demo-worker-pool" {
   integration_id = data.spacelift_aws_integration.this.id
   stack_id       = data.spacelift_stack.demo-worker-pool.id
   read           = true
