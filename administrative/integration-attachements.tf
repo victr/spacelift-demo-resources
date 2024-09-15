a13e1be30869300dd7bb58a3d5d4887eacc52913
jubran <jubrann@spacelift.io>
2024-09-14 19:01:02 -0500

    adds integration attachements

--- administrative/integration-attachements.tf
+++ administrative/integration-attachements.tf
@@ -4,3 +4,10 @@ resource "spacelift_aws_integration_attachment" "this" {
   read           = true
   write          = true
 }
+
+resource "spacelift_aws_integration_attachment" "this" {
+  integration_id = data.spacelift_aws_integration.this.id
+  stack_id       = data.spacelift_stack.demo-worker-pool.id
+  read           = true
+  write          = true
+}
