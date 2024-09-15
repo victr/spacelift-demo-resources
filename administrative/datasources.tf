d4f64f12ad4273dfe6045739162102de1cf79fa2
jubran <jubrann@spacelift.io>
2024-09-14 20:24:24 -0500

    cleans up part 2

--- administrative/datasources.tf
+++ administrative/datasources.tf
@@ -1,9 +1,1 @@
 data "spacelift_aws_integrations" "this" {}
-
-# data "spacelift_stack" "ec2-deployment" {
-#   stack_id = "ec2-deployment"
-# }
-
-# data "spacelift_stack" "demo-worker-pool" {
-#   stack_id = "demo-worker-pool"
-# }
