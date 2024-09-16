287e70a8e7b745cf7e0770438a24de811773d60b
jubran <jubrann@spacelift.io>
2024-09-15 22:40:45 -0500

    fixes uuid interpolation

--- s3-deployment/main.tf
+++ s3-deployment/main.tf
@@ -1,7 +1,7 @@
 resource "random_uuid" "this" {}
 
 resource "aws_s3_bucket" "this" {
-  bucket = "spacelift-test-${random_uuid.this}"
+  bucket = "spacelift-test-${random_uuid.this.result}"
 
   tags = {
     Name        = "spacelift-tes-deployment"
