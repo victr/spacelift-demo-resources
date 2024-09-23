c09ebc638c4bc9f85a58d0280ca5b6a5fad9344c
jubran <jubrann@spacelift.io>
2024-09-23 16:51:03 -0500

    fixes bug

--- administrative/blueprints.tf
+++ administrative/blueprints.tf
@@ -1,9 +1,9 @@
 locals {
-  switch_statement = format("%s : 'UNNKOWN'", join(" : ", [
+  switch_statement = join(".", [
     for integration in data.spacelift_aws_integrations.this.integrations :
-    format("inputs.integration == '%s' ? '%s'", integration.name, integration.integration_id)
-  ]))
-  func_integrations_switch_statement = format("$${{ %s }}", local.switch_statement)
+    format("replace('%s', '%s')", integration.name, integration.integration_id)
+  ])
+  func_integrations_switch_statement = format("$${{ inputs.integration.%s }}", local.switch_statement)
 }
 
 resource "spacelift_blueprint" "AWS-environment" {
